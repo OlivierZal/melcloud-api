c6207bcacc66f6ec2725c25d8553033ef18f43af
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-06-14 15:45:51 +0200

    better

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,8 @@
-export { default as AreaModel, type AreaModelAny } from './area'
+export type { AreaModelAny } from './area'
+export { default as AreaModel } from './area'
 export { default as BuildingModel } from './building'
-export { default as DeviceModel, type DeviceModelAny } from './device'
+export type { DeviceModelAny } from './device'
+export { default as DeviceModel } from './device'
 export { default as FloorModel } from './floor'
 export type {
   IAreaModel,
