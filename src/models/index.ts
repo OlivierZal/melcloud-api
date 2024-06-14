858ad65014c2d5fe784360763d01c8752577b780
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-06-14 17:21:29 +0200

    deduplicate exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,6 @@
-export type { AreaModelAny } from './area'
-export { default as AreaModel } from './area'
+export { default as AreaModel, type AreaModelAny } from './area'
 export { default as BuildingModel } from './building'
-export type { DeviceModelAny } from './device'
-export { default as DeviceModel } from './device'
+export { default as DeviceModel, type DeviceModelAny } from './device'
 export { default as FloorModel } from './floor'
 export type {
   IAreaModel,
