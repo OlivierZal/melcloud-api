4c20eea296b65522cb62846cf1ede7716afab497
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-06-18 22:13:53 +0200

    fix

--- src/facades/building.ts
+++ src/facades/building.ts
@@ -1,3 +1,4 @@
+import type API from '../services'
 import BaseSuperDeviceFacade from './base_super_device'
 import { BuildingModel } from '../models'
 import type { BuildingSettings } from '../types'
@@ -11,16 +12,21 @@ export default class
 
   protected readonly holidayModeLocation = 'Buildings'
 
-  protected override readonly isFrostProtectionDefined =
-    this.model.data.FPDefined
+  protected override readonly isFrostProtectionDefined
 
-  protected override readonly isHolidayModeDefined = this.model.data.HMDefined
+  protected override readonly isHolidayModeDefined
 
   protected readonly modelClass = BuildingModel
 
   protected readonly setAtaGroupSpecification = 'BuildingID'
 
   protected readonly tableName = 'Building'
+
+  public constructor(api: API, id: number) {
+    super(api, id)
+    this.isFrostProtectionDefined = this.data.FPDefined
+    this.isHolidayModeDefined = this.data.HMDefined
+  }
 
   public get data(): BuildingSettings {
     return this.model.data
