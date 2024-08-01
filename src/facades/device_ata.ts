38d41544425b6780810f3487df0ea8620c5949b0
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-08-02 00:28:42 +0200

    improve GH actions

--- src/facades/device_ata.ts
+++ src/facades/device_ata.ts
@@ -45,14 +45,12 @@ export default class extends BaseDeviceFacade<'Ata'> {
           max: this.data.MaxTempAutomatic,
           min: this.data.MinTempAutomatic,
         }
-      /* eslint-disable perfectionist/sort-switch-case */
       case OperationMode.cool:
       case OperationMode.dry:
         return { max: this.data.MaxTempCoolDry, min: this.data.MinTempCoolDry }
       case OperationMode.heat:
       default:
         return { max: this.data.MaxTempHeat, min: this.data.MinTempHeat }
-      /* eslint-enable perfectionist/sort-switch-case */
     }
   }
 
