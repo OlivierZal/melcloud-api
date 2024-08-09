d6e307029e20ee292519735905c1e5fc336e8b7f
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-08-09 12:04:02 +0200

    Add version to documentation

--- src/facades/device_erv.ts
+++ src/facades/device_erv.ts
@@ -7,17 +7,17 @@ export default class extends BaseDeviceFacade<'Erv'> {
   @alias('SetFanSpeed')
   public accessor fan: unknown = null
 
-  @alias('Power')
-  public accessor power: unknown = null
-
-  @alias('PM25Level')
-  public accessor pm25: unknown = null
-
   @alias('VentilationMode')
   public accessor mode: unknown = null
 
   @alias('OutdoorTemperature')
   public accessor outdoorTemperature: unknown = null
+
+  @alias('PM25Level')
+  public accessor pm25: unknown = null
+
+  @alias('Power')
+  public accessor power: unknown = null
 
   @alias('RoomTemperature')
   public accessor temperature: unknown = null
