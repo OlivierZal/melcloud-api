148538baefb30614aabb97fbde62b48eb8ac9ba7
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-08-15 19:29:07 +0200

    re-add missing rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -172,6 +172,7 @@ export default [
         'no-ternary': 'off',
         'no-undefined': 'off',
         'one-var': ['error', 'never'],
+        'perfectionist/sort-array-includes': 'error',
         'perfectionist/sort-classes': [
           'error',
           {
@@ -309,8 +310,11 @@ export default [
             ],
           },
         ],
-        'perfectionist/sort-interfaces': 'off',
+        'perfectionist/sort-enums': 'error',
+        'perfectionist/sort-exports': 'error',
+        'perfectionist/sort-imports': 'error',
         'perfectionist/sort-intersection-types': ['error', sortTypeOptions],
+        'perfectionist/sort-maps': 'error',
         'perfectionist/sort-named-exports': [
           'error',
           sortNamedExportImportOptions,
@@ -319,7 +323,7 @@ export default [
           'error',
           sortNamedExportImportOptions,
         ],
-        'perfectionist/sort-object-types': 'off',
+        'perfectionist/sort-switch-case': 'error',
         'perfectionist/sort-union-types': ['error', sortTypeOptions],
         'sort-imports': 'off',
         'sort-keys': [
