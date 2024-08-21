24a294ed6ad545eb2719a66dd55a39a95a614acf
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-08-22 00:35:05 +0200

    improve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -277,6 +277,7 @@ export default [
               'private-method',
               'abstract-method',
               'abstract-protected-method',
+              'unknown',
             ],
           },
         ],
