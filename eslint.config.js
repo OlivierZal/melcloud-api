afc52039437b1666356efcc2c2e858ec689f4ad3
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-12-20 00:06:52 +0100

    bump modules

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,10 @@ import ts, { configs as tsConfigs } from 'typescript-eslint'
 
 import { classGroups } from './eslint-utils/class-groups.js'
 
+const arrayLikeGroups = {
+  groups: ['literal', 'spread'],
+}
+
 const decoratorGroups = {
   customGroups: {
     'fetch-decorator': '^fetchDevices$',
@@ -294,7 +298,7 @@ const config = [
         'no-ternary': 'off',
         'no-undefined': 'off',
         'one-var': ['error', 'never'],
-        'perfectionist/sort-array-includes': 'error',
+        'perfectionist/sort-array-includes': ['error', arrayLikeGroups],
         'perfectionist/sort-classes': ['error', classGroups],
         'perfectionist/sort-decorators': ['error', decoratorGroups],
         'perfectionist/sort-enums': 'error',
@@ -309,7 +313,7 @@ const config = [
         'perfectionist/sort-named-imports': ['error', valuesFirst],
         'perfectionist/sort-object-types': ['error', typeLikeGroups],
         'perfectionist/sort-objects': 'error',
-        'perfectionist/sort-sets': 'error',
+        'perfectionist/sort-sets': ['error', arrayLikeGroups],
         'perfectionist/sort-switch-case': 'error',
         'perfectionist/sort-union-types': ['error', typeGroups],
         'sort-imports': 'off',
