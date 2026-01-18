14324594217ea727482ee81b99e91c34fbc59126
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2026-01-19 00:08:26 +0100

    improve

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,12 @@ import stylistic from '@stylistic/eslint-plugin'
 import prettier from 'eslint-config-prettier/flat'
 import perfectionist from 'eslint-plugin-perfectionist'
 import unicorn from 'eslint-plugin-unicorn'
-import yml from 'eslint-plugin-yml'
 
 import { defineConfig } from 'eslint/config'
 import { flatConfigs as importXConfigs } from 'eslint-plugin-import-x'
 import { configs as packageJsonConfigs } from 'eslint-plugin-package-json'
 import { Alphabet } from 'eslint-plugin-perfectionist/alphabet'
+import { configs as ymlConfigs } from 'eslint-plugin-yml'
 import { configs as tsConfigs } from 'typescript-eslint'
 
 import { classGroups } from './eslint-utils/class-groups.js'
@@ -456,38 +456,41 @@ const config = defineConfig([
     },
   },
   {
-    extends: [yml.configs['flat/standard'], yml.configs['flat/prettier']],
-    rules: {
-      'yml/file-extension': [
-        'error',
-        {
-          extension: 'yml',
-        },
-      ],
-      'yml/require-string-key': 'error',
-      'yml/sort-keys': [
-        'error',
-        {
-          order: {
-            caseSensitive: true,
-            natural: true,
-            type: 'asc',
-          },
-          pathPattern: '^.*$',
-        },
-      ],
-      'yml/sort-sequence-values': [
-        'error',
-        {
-          order: {
-            caseSensitive: true,
-            natural: true,
-            type: 'asc',
-          },
-          pathPattern: '^.*$',
-        },
-      ],
-    },
+    extends: [ymlConfigs.standard, ymlConfigs.prettier],
+
+    /*
+     * Rules: {
+     *   'yml/file-extension': [
+     *     'error',
+     *     {
+     *       extension: 'yml',
+     *     },
+     *   ],
+     *   'yml/require-string-key': 'error',
+     *   'yml/sort-keys': [
+     *     'error',
+     *     {
+     *       order: {
+     *         caseSensitive: true,
+     *         natural: true,
+     *         type: 'asc',
+     *       },
+     *       pathPattern: '^.*$',
+     *     },
+     *   ],
+     *   'yml/sort-sequence-values': [
+     *     'error',
+     *     {
+     *       order: {
+     *         caseSensitive: true,
+     *         natural: true,
+     *         type: 'asc',
+     *       },
+     *       pathPattern: '^.*$',
+     *     },
+     *   ],
+     * },
+     */
   },
   {
     extends: [
